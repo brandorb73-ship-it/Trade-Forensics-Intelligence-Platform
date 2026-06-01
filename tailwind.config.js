f74be00ff3669b0f9751f6ece2c490ddea1ccb37
brandorb73-ship-it <brandorb73@gmail.com>
2026-06-01 21:45:45 +1000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          900: '#0B0F19', // Deep Forensic Obsidian
+          800: '#151B2C', // Control Cards
+          700: '#222B45', // Borders & Panel Seams
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      }
+    },
+  },
+  plugins: [],
+}
